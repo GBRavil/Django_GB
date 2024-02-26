36b7ddf4d64490bf227005b231638e514c182d4a
Ravil <newromeo2006@yandex.ru>
2024-02-26 17:29:41 +0300

    hw_3

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp1.urls'))
+    path('', include('myapp1.urls')),
+    path('myapp3/', include('myapp3.urls'))
 ]

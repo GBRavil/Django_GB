8aba3c7a5ae50eb9afcb6a5562122b860d93d197
Ravil <newromeo2006@yandex.ru>
2024-03-08 12:30:43 +0300

    HW_6

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -32,7 +32,8 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    "192.168.0.109",
+    # "192.168.0.109",
+    'rav24.pythonanywhere.com',
 ]
 
 INTERNAL_IPS = [
@@ -94,8 +95,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rav24$default',
+        'USER': 'rav24',
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': 'rav24.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -135,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
